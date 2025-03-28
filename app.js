6f4cbbc9fba17eae7710a5e5abca62dc503b3be3
JDemmerle <jonathan.demmerle@web.de>
2025-03-28 21:16:11 +0100

    Remove unused code

--- app.js
+++ app.js
@@ -30,13 +30,33 @@ const generateRandomString = (length) => {
 var stateKey = 'spotify_auth_state';
 
 var app = express();
+const HEADERS = {
+  "Content-Security-Policy":
+    "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+  "Cross-Origin-Opener-Policy": "same-origin",
+  "Cross-Origin-Resource-Policy": "same-origin",
+  "Origin-Agent-Cluster": "?1",
+  "Referrer-Policy": "no-referrer",
+  "Strict-Transport-Security": "max-age=15552000; includeSubDomains",
+  "X-Content-Type-Options": "nosniff",
+  "X-DNS-Prefetch-Control": "off",
+  "X-Download-Options": "noopen",
+  "X-Frame-Options": "SAMEORIGIN",
+  "X-Permitted-Cross-Domain-Policies": "none",
+  "X-XSS-Protection": "0",
+};
 
 app.use(express.static(__dirname + '/public'))
    .use(cors())
    .use(cookieParser());
+  //  .use(helmet());
+
+
+app.disable("x-powered-by");
+
 
 app.get('/login', function(req, res) {
-
+  res.set(HEADERS);
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
 
@@ -88,17 +108,6 @@ app.get('/callback', function(req, res) {
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
 
-        // var options = {
-        //   url: 'https://api.spotify.com/v1/me',
-        //   headers: { 'Authorization': 'Bearer ' + access_token },
-        //   json: true
-        // };
-
-        // use the access token to access the Spotify Web API
-        // request.get(options, function(error, response, body) {
-        //   console.log(body);
-        // });
-
         // we can also pass the token to the browser to make requests from there
         res.redirect('/#' +
           querystring.stringify({
@@ -143,5 +152,5 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-console.log('Listening on 8888');
+console.log('Listening on '+listen_host+':8888');
 app.listen(8888, listen_host);
